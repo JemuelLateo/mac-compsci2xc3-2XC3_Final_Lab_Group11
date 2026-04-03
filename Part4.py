997a116b1330b91d84838d1e96524a22a4961611
kkh7788 <haokaihang2021@126.com>
2026-04-03 07:15:27 -0400

    Update Part4.py

--- Part4.py
+++ Part4.py
@@ -1,9 +1,14 @@
 from final_project_part1 import DirectedWeightedGraph, dijkstra, bellman_ford
-from Part2 import a_star
+from Part3 import (
+    a_star,
+    load_stations,
+    load_connections,
+    build_london_graph,
+    build_heuristic,
+)
 
 
 class Graph(DirectedWeightedGraph):
- 
 
 
     def get_adj_nodes(self, node):
@@ -14,12 +19,10 @@ class Graph(DirectedWeightedGraph):
 
 
 class WeightedGraph(Graph):
-
     pass
 
 
 class HeuristicGraph(WeightedGraph):
-
 
     def __init__(self):
         super().__init__()
@@ -31,50 +34,79 @@ class HeuristicGraph(WeightedGraph):
     def get_heuristic(self):
         return self.heuristic
 
+    @classmethod
+    def from_london_data(cls, stations_file, connections_file, destination):
+        stations = load_stations(stations_file)
+        connections = load_connections(connections_file)
+
+        base_graph = build_london_graph(stations, connections)
+        heuristic = build_heuristic(stations, destination)
+
+        graph = cls()
+        graph.adj = base_graph.adj
+        graph.weights = base_graph.weights
+        graph.set_heuristic(heuristic)
+
+        # 方便以后扩展 / 写报告时用
+        graph.stations = stations
+        graph.connections = connections
+        graph.destination = destination
+
+        return graph
+
 
 class SPAlgorithm:
-
 
     def calc_sp(self, graph, source, dest):
         raise NotImplementedError()
 
 
 class Dijkstra(SPAlgorithm):
+
+
+    def __init__(self):
+        self.last_distance = None
+
     def calc_sp(self, graph, source, dest):
         dist = dijkstra(graph, source)
-        return dist.get(dest, float("inf"))
+        self.last_distance = dist.get(dest, float("inf"))
+        return self.last_distance
 
 
 class Bellman_Ford(SPAlgorithm):
+
+    def __init__(self):
+        self.last_distance = None
+
     def calc_sp(self, graph, source, dest):
         dist = bellman_ford(graph, source)
-        return dist.get(dest, float("inf"))
+        self.last_distance = dist.get(dest, float("inf"))
+        return self.last_distance
 
 
 class A_Star(SPAlgorithm):
 
-
     def __init__(self):
         self.last_pred = {}
         self.last_path = []
+        self.last_distance = None
 
     def calc_sp(self, graph, source, dest):
-        # TODO:
-  
         h = graph.get_heuristic()
-
         pred, path = a_star(graph, source, dest, h)
 
         self.last_pred = pred
         self.last_path = path
 
-        if path == []:
-            return float("inf")
+        if not path:
+            self.last_distance = float("inf")
+            return self.last_distance
 
         total = 0.0
         for i in range(len(path) - 1):
             total += graph.w(path[i], path[i + 1])
 
+        self.last_distance = total
         return total
 
     def get_last_path(self):
@@ -85,7 +117,6 @@ class A_Star(SPAlgorithm):
 
 
 class ShortPathFinder:
-
 
     def __init__(self):
         self.graph = None
@@ -105,3 +136,40 @@ class ShortPathFinder:
         return self.algorithm.calc_sp(self.graph, source, dest)
 
 
+if __name__ == "__main__":
+    
+    # London data demo
+    STATIONS_FILE = "london_stations.csv"
+    CONNECTIONS_FILE = "london_connections.csv"
+
+    source = 1
+    destination = 50
+
+    # heuristic
+    G = HeuristicGraph.from_london_data(
+        STATIONS_FILE,
+        CONNECTIONS_FILE,
+        destination
+    )
+
+    finder = ShortPathFinder()
+    finder.set_graph(G)
+
+    # Dijkstra
+    dij = Dijkstra()
+    finder.set_algorithm(dij)
+    dij_dist = finder.calc_short_path(source, destination)
+    print("Dijkstra distance:", dij_dist)
+
+    # Bellman-Ford
+    bf = Bellman_Ford()
+    finder.set_algorithm(bf)
+    bf_dist = finder.calc_short_path(source, destination)
+    print("Bellman-Ford distance:", bf_dist)
+
+    # A*
+    ast = A_Star()
+    finder.set_algorithm(ast)
+    ast_dist = finder.calc_short_path(source, destination)
+    print("A* distance:", ast_dist)
+    print("A* path:", ast.get_last_path())
